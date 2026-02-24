1446c7648006ce57b516419f20b6d447e3ebf7a9
nharax <james.nharayakoma@aizatron.com>
2026-02-24 22:33:12 +0200

    Added Node.js test lambda

---
+++ lambdas/node-test-lambda/index.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+    console.log("Event received:", JSON.stringify(event));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello from Node.js Lambda 🚀",
+            input: event
+        })
+    };
+};
