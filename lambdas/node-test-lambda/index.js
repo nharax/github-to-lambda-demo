f01a1db6a256031e801db93ec03f8a66abb1f989
nharax <144227343+nharax@users.noreply.github.com>
2026-03-02 15:13:48 +0200

    Update index.js
    Testing

--- lambdas/node-test-lambda/index.js
+++ lambdas/node-test-lambda/index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Hello from Node.js 2 Lambda 🚀",
+            message: "Hello from Node.js 2 Lambda - Walthrough Session 🚀",
             input: event
         })
     };
